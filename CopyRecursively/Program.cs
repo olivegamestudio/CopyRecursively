093231b64722997366344910f9238ffa72a54948
Simon Jefferies <simon.jefferies@ingg.com>
2020-09-23 13:06:47 +0100

    Take a pattern argument.

--- CopyRecursively/Program.cs
+++ CopyRecursively/Program.cs
@@ -9,21 +9,25 @@ namespace CopyRecursively
         {
             string path = args[0];
             string target = args[1];
+            string pattern = "*.*";
+            if (args.Length > 2)
+                pattern = args[2];
 
             if (!Directory.Exists(target))
             {
                 Directory.CreateDirectory(target);
             }
 
-            foreach (string filename in Directory.GetFiles(path, "*.nupkg", SearchOption.AllDirectories))
+            foreach (string filename in Directory.GetFiles(path, pattern, SearchOption.AllDirectories))
             {
                 string sourceFilename = Path.GetFileName(filename);
                 string sourcePath = Path.GetDirectoryName(filename);
+
                 if (sourcePath.ToLowerInvariant() != target.ToLowerInvariant())
                 {
                     string destPath = Path.Combine(target,sourceFilename);
                     File.Copy(filename, destPath, true);
-                    Console.WriteLine(filename +" copied to " +destPath);
+                    Console.WriteLine(filename +" copied to " + destPath);
                 }
             }
         }
