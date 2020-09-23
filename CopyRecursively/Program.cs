7eaa6a5aac89c484541ed27c1fa23d6cc123394a
Simon Jefferies <simon.jefferies@ingg.com>
2020-09-23 12:38:59 +0100

    Imported.

---
+++ CopyRecursively/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CopyRecursively
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            string target = args[1];
+
+            if (!Directory.Exists(target))
+            {
+                Directory.CreateDirectory(target);
+            }
+
+            foreach (string filename in Directory.GetFiles(path, "*.nupkg", SearchOption.AllDirectories))
+            {
+                string sourceFilename = Path.GetFileName(filename);
+                string sourcePath = Path.GetDirectoryName(filename);
+                if (sourcePath.ToLowerInvariant() != target.ToLowerInvariant())
+                {
+                    string destPath = Path.Combine(target,sourceFilename);
+                    File.Copy(filename, destPath, true);
+                    Console.WriteLine(filename +" copied to " +destPath);
+                }
+            }
+        }
+    }
+}
